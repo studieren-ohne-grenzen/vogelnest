af55f66c0b9e52415a97b4b48a130e19851ce874
Marcel Klehr <mklehr@gmx.net>
2019-08-11 14:06:09 +0200

    Fix syntax

--- config.py
+++ config.py
@@ -2,11 +2,11 @@
 LDAP_HOST = 'localhost'
 LDAP_PORT = 389
 
-DN_GROUPS = 'ou=groups,o=sog-de,dc=sog';
-DN_PEOPLE = 'ou=people,o=sog-de,dc=sog';
-DN_PEOPLE_ACTIVE = 'ou=active,ou=people,o=sog-de,dc=sog';
-DN_PEOPLE_INACTIVE = 'ou=inactive,ou=people,o=sog-de,dc=sog';
-DN_PEOPLE_GUESTS = 'ou=guests,ou=people,o=sog-de,dc=sog';
+DN_GROUPS = 'ou=groups,o=sog-de,dc=sog'
+DN_PEOPLE = 'ou=people,o=sog-de,dc=sog'
+DN_PEOPLE_ACTIVE = 'ou=active,ou=people,o=sog-de,dc=sog'
+DN_PEOPLE_INACTIVE = 'ou=inactive,ou=people,o=sog-de,dc=sog'
+DN_PEOPLE_GUESTS = 'ou=guests,ou=people,o=sog-de,dc=sog'
 
 BIND_DN = 'uid=dashboard,ou=services,dc=sog'
 BIND_PW = ''
